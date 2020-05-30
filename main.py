8fb61bcd1187b3377392828f8d6be23ec104e8b2
Prashant Singh <pk.airsqn@gmail.com>
2020-05-30 20:45:35 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+
+
+def CreateIfNotExist(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+def movefiles(foldername, file):
+    for file in files:
+        os.replace(file, f"{foldername}/{file}")    # to move files to folders
+
+files = os.listdir('/home/flyboypk/Downloads')
+# files.remove("main.py")
+
+CreateIfNotExist('Images')
+CreateIfNotExist('Docs')
+CreateIfNotExist('Media')
+CreateIfNotExist('Others')
+
+imgExists = [".png",".jpg",".jpeg"]
+images = [file for file in files if os.path.splitext(file)[1].lower() in imgExists ]
+
+docExists = [".txt",".docx",".doc",".pdf",".xls",".xlsx"]
+docs = [file for file in files if os.path.splitext(file)[1].lower() in docExists ]
+
+
+movefiles("Images", images)
+movefiles("Docs", docs)
