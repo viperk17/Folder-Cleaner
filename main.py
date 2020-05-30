a891444997b45e33140d550d72753b5190776c5a
Prashant Singh <pk.airsqn@gmail.com>
2020-05-30 20:49:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ files = os.listdir('/home/flyboypk/Downloads')
 
 CreateIfNotExist('Images')
 CreateIfNotExist('Docs')
+CreateIfNotExist('Media')
+CreateIfNotExist('Others')
+
 
 imgExists = [".png",".jpg",".jpeg"]
 images = [file for file in files if os.path.splitext(file)[1].lower() in imgExists ]
@@ -21,6 +24,16 @@ images = [file for file in files if os.path.splitext(file)[1].lower() in imgExis
 docExists = [".txt",".docx",".doc",".pdf",".xls",".xlsx"]
 docs = [file for file in files if os.path.splitext(file)[1].lower() in docExists ]
 
+mediaExists = [".mp3",".mp4",".mkv",".flv"]
+medias = [file for file in files if os.path.splitext(file)[1].lower() in mediaExists ]
+
+others = []
+    for file in files:
+        exists = os.path.splitext(file)[1].lower()
+        if (exists not in mediaExists) and (exists not in docExists) and (exists not in imgExists) and os.path.isfile(file):
+            others.append(file)
 
 movefiles("Images", images)
 movefiles("Docs", docs)
+movefiles("Media", medias)
+movefiles("Others", others)
